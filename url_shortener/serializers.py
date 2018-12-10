d92884aab2db5093eec0a0d5876069fd6e3b714e
Gulnara Ibragimova <igulnara6@gmail.com>
2018-12-10 13:39:08 +0500

    Delete unused serializer

--- url_shortener/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from url_shortener.models import Url
-
-
-class UrlSerializer(serializers.Serializer):
-    class Meta:
-        model = Url
-        fields = ('__all__',)
